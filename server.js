a499b80c8247b28f9fe5e1f2a64eaaa66c4787ba
Ruth-Kilian <127222133+Ruth-Kilian@users.noreply.github.com>
2023-06-14 15:44:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,15 @@
 
 const app = require("./app");
 const port = process.env.PORT || 8080;
+const express = require("express");
+const path = require('path');
+
+// this code sets up a static file server that serves the frontend files from the "build" directroy
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+// it ensures all that all requests to the backend are redirected to the index.html file in the frontend folder
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
+});
 
 app.listen(port, () => console.log("Server started on port 8080"));
